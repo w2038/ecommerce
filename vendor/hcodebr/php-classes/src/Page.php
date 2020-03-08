15f84b1a19ec38256c3150b6849c7a9a5fbb8db1
w2038 <w4gn3r2038@gmail.com>
2020-03-08 11:12:12 -0300

    site admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -14,56 +14,48 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array())
-	{
+	public function __construct($opts = array(), $tpl_dir ="/views/"){
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-		    "base_url"      => null,
-		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
 		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 		    "debug"         => false
 		);
 
 		Tpl::configure( $config );
 
-		$this->tpl = new Tpl();
+		$this->tpl = new Tpl;
 
-		if ($this->options['data']) $this->setData($this->options['data']);
+		$this->setData($this->options['data']);
 
-		if ($this->options['header'] === true) $this->tpl->draw("header", false);
-
-	}
-
-	public function __destruct()
-	{
-
-		if ($this->options['footer'] === true) $this->tpl->draw("footer", false);
+		$this->tpl->draw("header");
 
 	}
 
 	private function setData($data = array())
 	{
 
-		foreach($data as $key => $val)
-		{
-
-			$this->tpl->assign($key, $val);
-
+		foreach($data as $key => $value){
+			$this->tpl->assign($key, $value);
 		}
 
 	}
 
-	public function setTpl($tplname, $data = array(), $returnHTML = false)
+	public function setTpl($name, $data = array(), $returnHTML = false)
 	{
 
 		$this->setData($data);
 
-		return $this->tpl->draw($tplname, $returnHTML);
+		return $this->tpl->draw($name, $returnHTML);
 
 	}
 
+	public function __destruct(){
+
+		$this->tpl->draw("footer");
+	}
 }
 
  ?>
