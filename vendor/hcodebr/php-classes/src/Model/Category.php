b9b8e01487bc44b3702d8816d8a66eb99d31a145
w2038 <w4gn3r2038@gmail.com>
2020-04-01 21:06:43 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -24,7 +24,7 @@ class Category extends Model {
 		$sql = new Sql();
 
 		$results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
-			":idcategory"=>$this->getcategory(),
+			":idcategory"=>$this->getidcategory(),
 			":descategory"=>$this->getdescategory()
 		));
 
@@ -43,6 +43,8 @@ class Category extends Model {
 
 		$this->setData($results[0]);
 
+		Category::updateFile();
+
 	}
 
 	public function delete()
@@ -53,6 +55,23 @@ class Category extends Model {
 		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",[
 			':idcategory'=>$this->getidcategory()
 		]);
+
+		Category::updateFile();
+	}
+
+	public static function updateFile()
+	{
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+
 	}
 	
 }
