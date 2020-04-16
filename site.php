0dc2174bca69f76b1b5242d783c4568171b7f49e
w2038 <w4gn3r2038@gmail.com>
2020-04-16 20:16:53 -0300

    Categorias vs produtos

--- site.php
+++ site.php
@@ -2,6 +2,7 @@
 
 use \Hcode\Page;
 use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
 
@@ -11,6 +12,20 @@ $app->get('/', function() {
 
 	$page->setTpl("index", [
 		'products'=>Product::checklist($products)
+	]);
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checklist($category->getProducts())
 	]);
 });
 
