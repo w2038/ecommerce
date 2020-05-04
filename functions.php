ac91a02590b454fc16c96d8dc14258557d27643e
w2038 <w4gn3r2038@gmail.com>
2020-05-03 21:43:50 -0300

    CEP com erro

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Hcode\Model\User;
+use \Hcode\Model\Cart;
 
 function formatPrice($vlprice)
 {
@@ -8,6 +9,13 @@ function formatPrice($vlprice)
     if (!$vlprice > 0) $vlprice = 0;
 
     return number_format($vlprice, 2, ",", ".");
+
+}
+
+function formatDate($date)
+{
+
+    return date('d/m/y', strtotime($date));
 
 }
 
